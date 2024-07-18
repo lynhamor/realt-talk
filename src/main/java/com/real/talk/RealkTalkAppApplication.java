408c8092bc8fe77098765214d09c7e5bbe93e47c
lhamor <liamlyndz@gmail.com>
2024-07-18 21:16:27 +0800

    initial commit

---
+++ src/main/java/com/real/talk/RealkTalkAppApplication.java
@@ -0,0 +1,13 @@
+package com.real.talk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealkTalkAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealkTalkAppApplication.class, args);
+	}
+
+}
